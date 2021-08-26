e1993f85a642e55c01f5b26ec357278ae83e3ce9
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-08-26 15:01:58 +0700

    learn array in typescript

---
+++ src/array.ts
@@ -0,0 +1,15 @@
+// ? Array
+let array1: number[];
+array1 = [1, 2, 3]
+
+let array2: string[];
+array2 = ["a", "b"]
+
+let array3: any[];
+array3 = [1, "b", [1, "b"]]
+
+// ? Tuples
+let biodata: [string, number];
+biodata = ["Jakarta", 123]
+// biodata = ["Jambi", true] //! Salah
+// biodata = ["Jakarta", 123, true] //! Salah
