5190b77417d565bdc6536d3956eaf51237f70536
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-08-26 15:02:23 +0700

    learn enum type in typescript

---
+++ src/enum.ts
@@ -0,0 +1,21 @@
+// ? Enum
+
+// ? Numeric Enum
+// enum Month {
+//     Jan = 1,
+//     Feb,
+//     Mar,
+//     Apr,
+//     May
+// }
+
+// ? String Enum
+enum Month {
+    Jan = "Januari",
+    Feb = "Februari",
+    Mar = "Maret",
+    Apr = "April",
+    May = "May"
+}
+
+console.log(Month);
