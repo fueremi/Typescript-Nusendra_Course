3fb028f1783bd81b4e6b5cdf57dc3d0e21dff845
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2021-08-26 15:01:41 +0700

    learning data type in typescript

---
+++ src/datatype.ts
@@ -0,0 +1,20 @@
+// ? String
+let nama: string = "Rian"
+nama = "title"
+
+// ? Number
+let umur: number = 25
+umur = 30
+
+// ? Boolean
+let statusPernikahan: boolean
+
+// ? Any 
+let heroes: any = "Iron Man"
+heroes = 20
+
+// ? Union
+// ! Contoh kasus, di nomor HP
+let phoneNumber: number | string;
+phoneNumber = 6285257718266
+phoneNumber = "+628 52 5771 8266"
